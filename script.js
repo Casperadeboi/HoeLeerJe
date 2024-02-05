46ed1caaa8893d265a0b2f36358bd32ba022a3e7
Casperadeboi <casperadeboi@gmail.com>
2024-02-05 20:16:25 +0100

    Nieuw gemaakt

--- script.js
+++ script.js
@@ -1,24 +1,52 @@
-
 document.addEventListener("DOMContentLoaded", function () {
-    // Voeg een 'click'-event toe aan elke navigatielink
     var links = document.querySelectorAll(".navbar a");
+
     links.forEach(function (link) {
         link.addEventListener("click", function (event) {
-            event.preventDefault(); // Voorkom standaardgedrag van de link
+            event.preventDefault();
 
-            // Haal de doel-ID op uit de href-attribuut van de link
             var targetId = this.getAttribute("href").substring(1);
+            var imageName = this.getAttribute("data-image");
 
-            // Verwijder de 'active'-klasse van alle containers
             document.querySelectorAll(".container").forEach(function (container) {
                 container.classList.remove("active");
             });
 
-            // Voeg de 'active'-klasse toe aan de container met het overeenkomstige ID
             document.getElementById(targetId).classList.add("active");
+            document.querySelector(".right-section img").src = imageName + ".png";
         });
     });
 
-    // Voeg 'active'-klasse toe aan de startpagina bij het laden van de pagina
     document.getElementById("startpagina").classList.add("active");
 });
+function showImage(word, imageName) {
+    document.getElementById(word).addEventListener("click", function () {
+        var imageContainer = document.createElement("div");
+        imageContainer.style.width = "100vw";
+        imageContainer.style.height = "100vh";
+        imageContainer.style.position = "fixed";
+        imageContainer.style.top = "0";
+        imageContainer.style.left = "0";
+        imageContainer.style.backgroundColor = "rgba(255, 255, 255, 0.9)";
+        imageContainer.style.zIndex = "9999";
+        imageContainer.style.display = "flex";
+        imageContainer.style.justifyContent = "center";
+        imageContainer.style.alignItems = "center";
+        imageContainer.style.overflow = "auto";
+
+        var image = document.createElement("img");
+        image.src = imageName;
+        image.style.width = "100%";
+        image.style.height = "100%";
+        image.style.objectFit = "contain"; // Voeg deze regel toe
+
+        imageContainer.appendChild(image);
+        document.body.appendChild(imageContainer);
+    });
+}
+
+showImage("ecoWord", "ECO.png");
+showImage("nlWord", "NL.png");
+showImage("akWord", "AK.png");
+showImage("maWord", "MA.png");
+showImage("wisWord", "WIS.png");
